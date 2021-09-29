fef38f4dac5f07167d32004d4cab4a6fd2a346a6
Pankaj Godbole <pankajsg@gmail.com>
2021-09-30 00:47:32 +0530

    Removed generated JS code

--- resources/public/js/prod/toolsdeps_figwheel_devcards/core.cljs
+++
@@ -1,8 +0,0 @@
-;; toolsdeps-figwheel-devcards/src/toolsdeps_figwheel_devcards/core.cljs
-
-(ns toolsdeps-figwheel-devcards.core)
-
-(defn -main
-  ""
-  [& args]
-  (println "In -main: args" args))
